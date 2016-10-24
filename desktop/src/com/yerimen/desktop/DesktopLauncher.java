3dd175372f27b7967e15bb97155780bee8bfaec7
Ignacio Laborde <laborde.ignacio@gmail.com>
2016-10-24 12:14:18 -0300

    added desktop icon

--- desktop/src/com/yerimen/desktop/DesktopLauncher.java
+++ desktop/src/com/yerimen/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.yerimen.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.yerimen.YerimenGame;
@@ -17,7 +18,7 @@ public class DesktopLauncher {
         config.fullscreen = false;
         config.vSyncEnabled = true;
         config.title = "Yerimen";
-
+        config.addIcon("../../desktop/images/icon.png", Files.FileType.Internal);
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
         config.width = (int) screenDimension.getWidth();
         config.height = (int) screenDimension.getHeight();
