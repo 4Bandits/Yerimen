b25dbf2b3d4caba7b7153062237b5187ef9e402b
Ignacio Laborde <laborde.ignacio@gmail.com>
2016-10-27 21:16:34 -0300

    fix error with batch

--- desktop/src/com/yerimen/desktop/DesktopLauncher.java
+++ desktop/src/com/yerimen/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.yerimen.desktop;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.yerimen.YerimenGame;
 
 import java.awt.*;
@@ -22,6 +23,10 @@ public class DesktopLauncher {
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
         config.width = (int) screenDimension.getWidth();
         config.height = (int) screenDimension.getHeight();
+
+        config.useGL30 = true;
+        ShaderProgram.prependVertexCode = "#version 140\n#define varying out\n#define attribute in\n";
+        ShaderProgram.prependFragmentCode = "#version 140\n#define varying in\n#define texture2D texture\n#define gl_FragColor fragColor\nout vec4 fragColor;\n";
         return config;
     }
 }
