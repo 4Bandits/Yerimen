1cf1c6d210ee9b3b302cbde835842b9eb089ac4f
Ignacio Laborde <laborde.ignacio@gmail.com>
2016-11-08 01:47:49 -0300

    replicado poder

--- core/src/com/yerimen/json/PowerJsonBuilder.java
+++ core/src/com/yerimen/json/PowerJsonBuilder.java
@@ -1,8 +1,8 @@
 package com.yerimen.json;
 
 import com.badlogic.gdx.math.Vector2;
-import com.yerimen.powers.FireBall;
 import com.yerimen.powers.Power;
+import com.yerimen.powers.PowerFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,49 +12,49 @@ public class PowerJsonBuilder {
     private String attackID;
     private Float x;
     private Float y;
-    private String name;
+    private String type;
     private Float velocity;
     private Float distance;
     private Float destinationX;
     private Float destinationY;
 
-    public PowerJsonBuilder(Power power){
+    public PowerJsonBuilder(Power power) {
         this.characterID = power.getCharacterID();
         this.attackID = power.getAttackID();
         this.x = power.getX();
         this.y = power.getY();
-        this.name = "images/fireBall.png";
+        this.type = power.getType();
         this.velocity = power.getVelocity();
         this.distance = power.getDistance();
         this.destinationX = power.getDestination().x;
         this.destinationY = power.getDestination().y;
     }
 
-    public JSONObject build(){
+    public JSONObject build() {
         JSONObject data = new JSONObject();
         try {
             data.put("characterID", characterID);
             data.put("attackID", attackID);
             data.put("x", x);
             data.put("y", y);
-            data.put("name", name);
+            data.put("type", type);
             data.put("velocity", velocity);
             data.put("distance", distance);
             data.put("destinationX", destinationX);
             data.put("destinationY", destinationY);
             return data;
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error - Json");
         }
     }
 
-    public PowerJsonBuilder(JSONObject jsonObject){
+    public PowerJsonBuilder(JSONObject jsonObject) {
         try {
             characterID = jsonObject.getString("characterID");
             attackID = jsonObject.getString("attackID");
             x = new Float(jsonObject.getDouble("x"));
             y = new Float(jsonObject.getDouble("y"));
-            name = jsonObject.getString("name");
+            type = jsonObject.getString("type");
             velocity = new Float(jsonObject.getDouble("velocity"));
             distance = new Float(jsonObject.getDouble("distance"));
             destinationX = new Float(jsonObject.getDouble("destinationX"));
@@ -64,7 +64,7 @@ public class PowerJsonBuilder {
         }
     }
 
-    public Power buildObject(){
-        return new FireBall(this.characterID, this.attackID, this.distance, new Vector2(destinationX, destinationY), new Vector2(x,y));
+    public Power buildObject() {
+        return PowerFactory.getPower(this.characterID, this.attackID, this.distance, new Vector2(destinationX, destinationY), new Vector2(x, y), type);
     }
 }
