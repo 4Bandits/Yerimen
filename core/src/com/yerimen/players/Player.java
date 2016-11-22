996381d02138ccd96f8646b2967e4849646460c8
Alvaro <alvaropiorno100@gmail.com>
2016-11-21 23:51:10 -0300

    mas intentos de collision de mapa

--- core/src/com/yerimen/players/Player.java
+++ core/src/com/yerimen/players/Player.java
@@ -3,7 +3,11 @@ package com.yerimen.players;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.yerimen.powers.FireBall;
@@ -14,16 +18,22 @@ import com.yerimen.textures.TextureManager;
 
 public class Player extends Character implements Observable {
     TiledMapTileLayer collisionObjectLayer ;
+    float tileHeight, tileWidth;
+    float oldX, oldY;
     public Player(PlayerTexture playerTexture, CharacterStatus playerStatus, Vector2 position) {
         super(playerTexture, playerStatus, position);
         collisionObjectLayer =(TiledMapTileLayer)TextureManager.getInstance().getMap().getLayers().get("Blocking prueba");
+        tileHeight=collisionObjectLayer.getTileHeight();
+        tileWidth=collisionObjectLayer.getTileWidth();
+         oldX=getXPosition();
+         oldY=getYPosition();
     }
 
     @Override
     public void update(float delta, OrthographicCamera camera) {
         stateTime += delta;
-        detectCollisionsWithMapObjects();
         processMove();
+        detectCollisionsWithMapObjects();
 
         if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
             Vector3 mousePosition = this.getMousePosition(camera, Gdx.input.getX(), Gdx.input.getY());
@@ -76,7 +86,7 @@ public class Player extends Character implements Observable {
     }
     private boolean isCellBlocked(float x, float y){
         TiledMapTileLayer.Cell cell= collisionObjectLayer.getCell((int)(x/collisionObjectLayer.getTileWidth()),(int)(y/collisionObjectLayer.getTileHeight()));
-        return cell!=null && cell.getTile()!=null && cell.getTile().getProperties().containsKey("block");
+        return cell!=null && cell.getTile()!=null && cell.getTile().getProperties().containsKey("block")&&(boolean)cell.getTile().getProperties().get("block");
     }
     private boolean isHeadingUpAndRight() {
         return (Gdx.input.isKeyPressed(Input.Keys.UP) ||
@@ -143,90 +153,74 @@ public class Player extends Character implements Observable {
                         Gdx.input.isKeyPressed(Input.Keys.S)));
     }
     protected void detectCollisionsWithMapObjects() {
-        //int objectLayerId = 3;
-        //Rectangle rectPlayer = new Rectangle();
-        //rectPlayer.set(getXPosition(),getYPosition(),getSprite().getWidth(),getSprite().getHeight());
+        int objectLayerId = 3;
+        Rectangle rectPlayer = new Rectangle();
+        Rectangle rect = new Rectangle();
+        rectPlayer.set(getXPosition(),getYPosition(),getSprite().getWidth(),getSprite().getHeight());
         //TiledMapTileLayer collisionObjectLayer =(TiledMapTileLayer)TextureManager.getInstance().getMap().getLayers().get(objectLayerId);
-       // MapObjects objects = collisionObjectLayer.getObjects();
-        float oldX=getXPosition();
-        float oldY=getYPosition();
-        boolean collision=false;
 
-        if(isHeadingUpAndRight()){
-            collision= isCellBlocked(getXPosition()+getSprite().getWidth(),getYPosition()+getSprite().getHeight());
+        MapObjects objects = collisionObjectLayer.getObjects();
+        if (isCellBlocked(getXPosition(),getYPosition())){
+            this.getSprite().setPosition(500,500);
         }
-        if(isHeadingUpAndLeft()){
-           collision= isCellBlocked(getXPosition(),getYPosition()+getSprite().getHeight());
-        }
-        if(isHeadingDownAndRight()){
-            collision= isCellBlocked(getXPosition()+getSprite().getWidth(),getYPosition());
-        }
-        if(isHeadingDownAndLeft()){
-            collision= isCellBlocked(getXPosition(),getYPosition());
-        }
-        if(isHeadingUp()){
-            collision= isCellBlocked(getXPosition()+getSprite().getWidth()/2,getYPosition()+getSprite().getHeight());
-        }
-        if(isHeadingDown()){
-            collision= isCellBlocked(getXPosition()+getSprite().getWidth()/2,getYPosition());
-        }
-        if(isHeadingLeft()){
-            collision= isCellBlocked(getXPosition(),getYPosition()+getSprite().getHeight()/2);
-        }
-        if(isHeadingRight()){
-            collision= isCellBlocked(getXPosition()+getSprite().getWidth(),getYPosition()+getSprite().getHeight()/2);
-        }
+/*      */
 
-        if(collision){
-            this.getSprite().setPosition(oldX,oldY);
-        }
-        /*
         //TiledMapTileLayer.Cell object;
         //Rectangle rect = new Rectangle();
-        for (int y = 0; y <= collisionObjectLayer.getHeight(); y++) {
+       /* for (int y = 0; y <= collisionObjectLayer.getHeight(); y++) {
             for (int x = 0; x <= collisionObjectLayer.getWidth(); x++) {
                 TiledMapTileLayer.Cell cell = collisionObjectLayer.getCell(x, y);
-                if (cell != null) {
-                   rect.set(x,y,1,1);
-                    if(rect.overlaps(getSprite().getBoundingRectangle())){
-                        getSprite().setPosition(1,1);
+                if (isCellBlocked(x,y) && isCollidingWithCellIn(x,y) ) {
+                   /*rect.set(x,y,tileHeight,tileWidth);
+                    if(rect.overlaps(rectPlayer)){
+                        getSprite().setPosition(500,500);
                     }
                 }
             }
-        }
+        }*/
 
-*/
-
-
-
-        /*switch (getDirection()){
-            case "up":
-                rect.set(getSprite().getX(),getSprite().getY()+1,1,1);
-
-            case "down":
-                rect.set(getSprite().getX(),getSprite().getY()-1,1,1);
-
-            case "left":
-                rect.set(getSprite().getX()-1,getSprite().getY(),1,1);
-            case "right":
-                rect.set(getSprite().getX()+1,getSprite().getY(),1,1);
-        }
-         if(getSprite().getBoundingRectangle().overlaps(rect)){
-             this.getSprite().setX(0);
-             this.getSprite().setY(0);
-         }
-*/
         /*for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
             Rectangle rectangle = rectangleObject.getRectangle();
 
             if (rectangle.overlaps(rectPlayer)) {
 
-               this.getSprite().setX(0);
-                this.getSprite().setY(0);
+               this.getSprite().setPosition(0,0);
 
             }
 
         }*/
+    }
+
+    private boolean isCollidingWithCellIn(float x, float y){
+
+        boolean collision=false;
+
+        if(isHeadingUpAndRight()){
+            collision= getXPosition()+tileWidth ==x && getYPosition()+tileHeight ==y;
+        }
+        if(isHeadingUpAndLeft()){
+            collision= getXPosition()-tileHeight ==x && getYPosition()+tileHeight ==y;
+        }
+        if(isHeadingDownAndRight()){
+            collision= getXPosition()+tileWidth ==x && getYPosition()-tileHeight ==y;
+        }
+        if(isHeadingDownAndLeft()){
+            collision= getXPosition()+tileWidth ==x && getYPosition()+tileHeight ==y;
+        }
+        if(isHeadingUp()){
+            collision= getXPosition() ==x && getYPosition()+tileHeight ==y;
+        }
+        if(isHeadingDown()){
+            collision= getXPosition() ==x && getYPosition()-tileHeight ==y;
+        }
+        if(isHeadingLeft()){
+            collision= getXPosition()-tileWidth ==x && getYPosition() ==y;
+        }
+        if(isHeadingRight()){
+            collision= getXPosition()+tileWidth ==x && getYPosition() ==y;
+        }
+
+        return collision;
     }
 
 }
